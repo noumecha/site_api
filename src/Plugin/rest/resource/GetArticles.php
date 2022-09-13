0de2d26d48a648896970758c3c71f13ba984c36f
noumecha spaker <noumechaivan@gmail.com>
2022-09-13 16:55:24 +0100

    maj : add the get method

--- src/Plugin/rest/resource/GetArticles.php
+++ src/Plugin/rest/resource/GetArticles.php
@@ -24,7 +24,23 @@ class GetArticles extends ResourceBase {
      * @return \Drupal\rest\ResourceResponse
      */
     public function get() {
-      $response = ['message' => 'Hello, this is a rest service for durpal site'];
+      //$response = ['message' => 'Hello, this is a rest service for durpal site'];
+      $nids = \Drupal::entityQuery('node')->condition('type','article')->execute();
+      $nodes =  \Drupal\node\Entity\Node::loadMultiple($nids);
+      $response = $this->processNode($nodes);
       return new ResourceResponse($response);
+    }
+
+    /**
+     * Get Articles
+     */
+    private function processNode($nodes) 
+    {
+      $output = [];
+      foreach($nodes as $key => $node)
+      {
+        $ouput[$key]['title'] = $node->get('title')->getValue();
+      }
+      return $output;
     }
   }
